74dfeea0eed289cbb5bf8147a428b4284117c6c5
tahsin-emre <telli@tahsinemre.com>
2021-06-18 22:06:19 +0300

    debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'Kaplumbağa Fanzin',
         home: Scaffold(
             appBar: AppBar(
+              centerTitle: true,
               title: Text('Kaplumbağa'),
             ),
             body: Center(child: Container(child: Text('Kaplumbağa - Hello')))));
